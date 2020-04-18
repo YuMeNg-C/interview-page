bac0b162b0537333f0d2d43cf1b41c8ce51aff26
sjy <1208057043@qq.com>
2020-04-18 17:21:56 +0800

    feat: bundle files

---
+++ build/precache-manifest.14093f97918675eaa7924e513be4b3ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4353745fdaac741672ed285cf76858d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "810c7833f9be277abde3",
+    "url": "/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "11f80a5a213bb00335b9",
+    "url": "/static/css/main.60e31372.chunk.css"
+  },
+  {
+    "revision": "810c7833f9be277abde3",
+    "url": "/static/js/2.84e4a8d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.84e4a8d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f80a5a213bb00335b9",
+    "url": "/static/js/main.a9219a2f.chunk.js"
+  },
+  {
+    "revision": "6802e2cebcfba87ab36e",
+    "url": "/static/js/runtime-main.01632480.js"
+  }
+]);
