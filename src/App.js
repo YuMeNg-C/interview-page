1974095c29ccb290d5ef80bd8779949079b50f89
sjy <1208057043@qq.com>
2020-04-18 17:12:20 +0800

    feat: drop list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { List, Avatar, Table } from 'antd'
+import axios from 'axios'
+import useInterval from './hooks/index'
 import './App.css';
 
-function App() {
+const columns = [
+  {
+    title: '姓名',
+    dataIndex: 'title',
+    key: 'title',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: '住址',
+    dataIndex: 'body',
+    key: 'body',
+  },
+  {
+    title: '年龄',
+    dataIndex: 'id',
+    key: 'id',
+  }
+]
+
+function App({props}) {
+
+  const [baseUrl, setBaseUrl] = useState('http://jsonplaceholder.typicode.com/posts')
+  const [list, setList] = useState([])
+  const [top, setTop] = useState(0)
+  const [drop, setDrop] = useState(true)
+  const [fetchCode, setFetchCode] = useState('pending')
+
+  useEffect(async () => {
+    const res = await axios.get(baseUrl)
+    setList(res.data)
+    setFetchCode('200')
+  }, [])
+
+  useInterval(() => { setTop(top - 2)}, 100, drop)
+
+  const inDom = () => {
+    setDrop(false)
+  }
+
+  const outDom = () => {
+    setDrop(true)
+  }
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        最新职位
+        <span style={{float: 'right', paddingRight: '10px'}}>更多</span>
+      </div>
+        <div className='list-wrapper' style={{top}} onMouseOver={inDom} onMouseLeave={outDom}>
+          {
+            fetchCode == "200" ? 
+            <Table
+              showHeader={true}
+              dataSource={props ? props : list }
+              columns={columns}
+              pagination={false}
+          /> : null
+          }
+      </div>
     </div>
   );
 }
---
+++ src/hooks/index.js
@@ -0,0 +1,17 @@
+import { useRef, useEffect } from 'react'
+
+export default function useInterval(callback, delay, drop) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    let id = setInterval(tick, delay);
+    return () => clearInterval(id);
+  }, [delay]);
+}
